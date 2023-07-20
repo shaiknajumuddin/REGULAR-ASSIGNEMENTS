a52d20b65b92d9c0394179e56bb9e6805557a0ae
shaiknajumuddin <131187819+shaiknajumuddin@users.noreply.github.com>
2023-07-20 09:41:29 +0530

    Delete week3 directory

--- week3/week2/day6/Bitwiseshift.java
+++
@@ -1,8 +0,0 @@
-class Bitwiseshift
-  {
-    public static void main(String args[])
-    {
-      System.out.println("left shift <<" +(22<<5));
-      System.out.println("right shift >>" +(22>>5));
-    }
-  }
